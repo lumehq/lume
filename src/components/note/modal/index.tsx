c45ad04d8751562c2ef231dde8abf6473d2c655d
Ren Amamiya <123083837+reyamir@users.noreply.github.com>
2023-02-24 08:06:50 +0700

    fixed note modal

--- src/components/note/modal/index.tsx
+++ src/components/note/modal/index.tsx
@@ -1,4 +1,3 @@
-import { UserWithUsername } from '@components/note/atoms/userWithUsername';
 import { Content } from '@components/note/content';
 import NoteReply from '@components/note/modal/noteReply';
 
@@ -12,7 +11,6 @@ const Modal = ({ event }: { event: any }) => {
       '#e': [event.id],
       since: event.created_at,
       kinds: [1],
-      limit: 100,
     },
   });
 
@@ -23,7 +21,6 @@ const Modal = ({ event }: { event: any }) => {
         <div className="relative z-10 h-full p-4">
           <div className="relative h-full overflow-auto rounded-lg border-[0.5px] border-white/30 bg-zinc-800 p-4 shadow-inner">
             <div className="flex flex-col gap-4">
-              <UserWithUsername pubkey={event.pubkey} />
               <Content data={event} />
             </div>
             <div className="flex flex-col gap-2 divide-y divide-zinc-700">
--- src/components/note/modal/noteReply.tsx
+++ src/components/note/modal/noteReply.tsx
@@ -1,18 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { User } from '@components/note/atoms/user';
 import { Content } from '@components/note/content';
 
 export default function NoteReply({ event }: { event: any }) {
   return (
     <div className="flex h-min min-h-min w-full select-text flex-col border-b border-zinc-800 py-4 hover:bg-zinc-800">
-      <div className="flex flex-col">
-        <User pubkey={event.pubkey} time={event.created_at} />
-        <div className="-mt-5 pl-[60px]">
-          <div className="flex flex-col gap-6">
-            <Content data={event.content} />
-          </div>
-        </div>
-      </div>
+      <Content data={event} />
     </div>
   );
 }
