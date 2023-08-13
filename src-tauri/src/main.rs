f8753eca90bc50149e5ae356f0e43b2201ac5b5e
Ren Amamiya <123083837+reyamir@users.noreply.github.com>
2023-08-13 12:03:00 +0700

    fix build errors

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -174,8 +174,8 @@ fn main() {
         .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
 
       #[cfg(target_os = "windows")]
-      apply_mica(&window, None, None)
-        .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+      apply_mica(&window, Some(true))
+        .expect("Unsupported platform! 'apply_mica' is only supported on Windows");
 
       Ok(())
     })
