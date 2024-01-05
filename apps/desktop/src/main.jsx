8e8e6fe244bd44efdd5a85efb1d0199a437dc233
reya <reya@lume.nu>
2024-01-05 07:31:08 +0700

    chore: restructure

--- apps/desktop/src/main.jsx
+++ apps/desktop/src/main.jsx
@@ -1,26 +1,8 @@
-import { LumeProvider } from '@lume/ark';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { createRoot } from 'react-dom/client';
-import { Toaster } from 'sonner';
-import App from './app';
-import './app.css';
+import { createRoot } from "react-dom/client";
+import App from "./app";
+import "./app.css";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retryDelay: (attemptIndex) => Math.min(1000 * 2 ** attemptIndex, 10000), // 10 seconds
-    },
-  },
-});
-
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 
-root.render(
-  <QueryClientProvider client={queryClient}>
-    <Toaster position="top-center" theme="system" closeButton />
-    <LumeProvider>
-      <App />
-    </LumeProvider>
-  </QueryClientProvider>
-);
+root.render(<App />);
