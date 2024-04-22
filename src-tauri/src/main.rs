17766d29d6125b705e62d2574bc88fd34f4fab57
reya <reya@lume.nu>
2024-04-22 13:18:34 +0700

    chore: update deps

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -81,7 +81,6 @@ fn main() {
       }
       _ => {}
     })
-    .plugin(tauri_plugin_store::Builder::default().build())
     .plugin(tauri_plugin_clipboard_manager::init())
     .plugin(tauri_plugin_dialog::init())
     .plugin(tauri_plugin_fs::init())
