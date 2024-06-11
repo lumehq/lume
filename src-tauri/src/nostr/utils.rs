f99394867ae13bd2c6551d663d1f4e2c2c9a0af6
xy <xyzmhx@gmail.com>
2024-06-12 00:49:10 +0900

    feat: improve parser performance

--- src-tauri/src/nostr/utils.rs
+++ src-tauri/src/nostr/utils.rs
@@ -10,117 +10,164 @@ use url::Url;
 
 #[derive(Debug, Serialize, Type)]
 pub struct Meta {
-	pub content: String,
-	pub images: Vec<String>,
-	pub videos: Vec<String>,
-	pub events: Vec<String>,
-	pub mentions: Vec<String>,
-	pub hashtags: Vec<String>,
+  pub content: String,
+  pub images: Vec<String>,
+  pub videos: Vec<String>,
+  pub events: Vec<String>,
+  pub mentions: Vec<String>,
+  pub hashtags: Vec<String>,
 }
 
-const NOSTR_EVENTS: [&str; 10] = ["@nevent1", "@note1", "@nostr:note1", "@nostr:nevent1", "nostr:note1", "note1", "nostr:nevent1", "nevent1", "Nostr:note1", "Nostr:nevent1"];
-const NOSTR_MENTIONS: [&str; 10] = ["@npub1", "nostr:npub1", "nostr:nprofile1", "nostr:naddr1", "npub1", "nprofile1", "naddr1", "Nostr:npub1", "Nostr:nprofile1", "Nostr:naddr1"];
+const NOSTR_EVENTS: [&str; 10] = [
+  "@nevent1",
+  "@note1",
+  "@nostr:note1",
+  "@nostr:nevent1",
+  "nostr:note1",
+  "note1",
+  "nostr:nevent1",
+  "nevent1",
+  "Nostr:note1",
+  "Nostr:nevent1",
+];
+const NOSTR_MENTIONS: [&str; 10] = [
+  "@npub1",
+  "nostr:npub1",
+  "nostr:nprofile1",
+  "nostr:naddr1",
+  "npub1",
+  "nprofile1",
+  "naddr1",
+  "Nostr:npub1",
+  "Nostr:nprofile1",
+  "Nostr:naddr1",
+];
 const IMAGES: [&str; 7] = ["jpg", "jpeg", "gif", "png", "webp", "avif", "tiff"];
 const VIDEOS: [&str; 5] = ["mp4", "mov", "avi", "webm", "mkv"];
 
 pub fn get_latest_event(events: &[Event]) -> Option<&Event> {
-	events.iter().max_by_key(|event| event.created_at())
+  events.iter().max_by_key(|event| event.created_at())
 }
 
 pub fn dedup_event(events: &[Event], nsfw: bool) -> Vec<Event> {
-	let mut seen_ids = HashSet::new();
-	let dedup = events.iter().filter(|event| {
-		let e = TagKind::SingleLetter(SingleLetterTag::lowercase(Alphabet::E));
-		let e_tags: Vec<&Tag> = event.tags.iter().filter(|el| el.kind() == e).collect();
-		let ids: Vec<&str> = e_tags.iter().map(|tag| tag.content().unwrap()).collect();
-		let is_dup = ids.iter().any(|id| seen_ids.contains(id));
+  let mut seen_ids = HashSet::new();
+  events
+    .iter()
+    .filter(|&event| {
+      let e = TagKind::SingleLetter(SingleLetterTag::lowercase(Alphabet::E));
+      let e_tags: Vec<&Tag> = event.tags.iter().filter(|el| el.kind() == e).collect();
+      let ids: Vec<&str> = e_tags.iter().filter_map(|tag| tag.content()).collect();
+      let is_dup = ids.iter().any(|id| seen_ids.contains(*id));
 
-		// Add found ids to seen list
-		for id in ids {
-			seen_ids.insert(id);
-		}
+      for id in &ids {
+        seen_ids.insert(*id);
+      }
 
-		// Filter NSFW event
-		if nsfw {
-			let w_tags: Vec<&Tag> = event.tags.iter().filter(|el| el.kind() == TagKind::ContentWarning).collect();
-			let is_warning = !w_tags.is_empty();
-			!is_dup && !is_warning
-		} else {
-			!is_dup
-		}
-	}).cloned().collect();
-
-	dedup
+      if nsfw {
+        let w_tags: Vec<&Tag> = event
+          .tags
+          .iter()
+          .filter(|el| el.kind() == TagKind::ContentWarning)
+          .collect();
+        !is_dup && w_tags.is_empty()
+      } else {
+        !is_dup
+      }
+    })
+    .cloned()
+    .collect()
 }
 
 pub async fn parse_event(content: &str) -> Meta {
-	let words = content.split_whitespace();
+  let words: Vec<_> = content.split_whitespace().collect();
+  let mut finder = LinkFinder::new();
+  finder.url_must_have_scheme(false);
+  let urls: Vec<_> = finder.links(content).collect();
 
-	// Link finder
-	let mut finder = LinkFinder::new();
-	finder.url_must_have_scheme(false);
+  let hashtags = words
+    .iter()
+    .filter(|&&word| word.starts_with('#'))
+    .map(|&s| s.to_string())
+    .collect::<Vec<_>>();
+  let events = words
+    .iter()
+    .filter(|&&word| NOSTR_EVENTS.iter().any(|&el| word.starts_with(el)))
+    .map(|&s| s.to_string())
+    .collect::<Vec<_>>();
+  let mentions = words
+    .iter()
+    .filter(|&&word| NOSTR_MENTIONS.iter().any(|&el| word.starts_with(el)))
+    .map(|&s| s.to_string())
+    .collect::<Vec<_>>();
 
-	// Get all urls
-	let urls: Vec<_> = finder.links(content).collect();
+  let mut images = Vec::new();
+  let mut videos = Vec::new();
+  let mut text = content.to_string();
 
-	// Extract hashtags
-	let hashtags = words.clone().filter(|word| word.starts_with('#')).collect::<Vec<_>>();
+  if !urls.is_empty() {
+    let client = Client::new();
 
-	// Extract nostr events
-	let events = words.clone().filter(|word| NOSTR_EVENTS.iter().any(|&el| word.starts_with(el))).collect::<Vec<_>>();
+    for url in urls {
+      let url_str = url.as_str();
 
-	// Extract nostr mentions
-	let mentions = words.clone().filter(|word| NOSTR_MENTIONS.iter().any(|&el| word.starts_with(el))).collect::<Vec<_>>();
+      if let Ok(parsed_url) = Url::from_str(url_str) {
+        if let Some(ext) = parsed_url
+          .path_segments()
+          .and_then(|segments| segments.last().and_then(|s| s.split('.').last()))
+        {
+          if IMAGES.contains(&ext) {
+            text = text.replace(url_str, "");
+            images.push(url_str.to_string());
+            break;
+          }
+          if VIDEOS.contains(&ext) {
+            text = text.replace(url_str, "");
+            videos.push(url_str.to_string());
+            break;
+          }
+        }
 
-	// Extract images and videos from content
-	let mut images = Vec::new();
-	let mut videos = Vec::new();
-	let mut text = content.to_string();
+        // Check the content type of URL via HEAD request
+        if let Ok(res) = client.head(url_str).send().await {
+          if let Some(content_type) = res.headers().get("Content-Type") {
+            if content_type.to_str().unwrap_or("").starts_with("image") {
+              text = text.replace(url_str, "");
+              images.push(url_str.to_string());
+              break;
+            }
+          }
+        }
+      }
+    }
+  }
 
-	if !urls.is_empty() {
-		let client = Client::new();
+  // Clean up the resulting content string to remove extra spaces
+  let cleaned_text = text.split_whitespace().collect::<Vec<_>>().join(" ");
 
-		for url in urls {
-			let url = url.as_str();
+  Meta {
+    content: cleaned_text,
+    events,
+    mentions,
+    hashtags,
+    images,
+    videos,
+  }
+}
 
-			if let Ok(parsed_url) = Url::from_str(url) {
-				if let Some(ext) = parsed_url.path_segments().and_then(|segments| segments.last()) {
-					let ext = ext.split('.').last().unwrap_or("");
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use nostr_sdk::{Event, EventBuilder, Kind, Timestamp};
 
-					if IMAGES.contains(&ext) {
-						text = text.replace(url, "");
-						images.push(url.to_string());
-						break;
-					}
+  #[tokio::test]
+  async fn test_parse_event() {
+    let content = "Check this image: https://example.com/image.jpg #cool @npub1";
+    let meta = parse_event(content).await;
 
-					if VIDEOS.contains(&ext) {
-						text = text.replace(url, "");
-						videos.push(url.to_string());
-						break;
-					}
-				}
-
-				// Check the content type of url via HEAD request
-				if let Ok(res) = client.head(url).send().await {
-					if let Some(content_type) = res.headers().get("Content-Type") {
-						if content_type.to_str().unwrap_or("").starts_with("image") {
-							text = text.replace(url, "");
-							images.push(url.to_string());
-							break;
-						}
-					}
-				}
-			}
-		}
-	}
-
-
-	Meta {
-		content: text.trim().to_string(),
-		events: events.iter().map(|&s| s.into()).collect(),
-		mentions: mentions.iter().map(|&s| s.into()).collect(),
-		hashtags: hashtags.iter().map(|&s| s.into()).collect(),
-		images,
-		videos,
-	}
+    assert_eq!(meta.content, "Check this image: #cool @npub1");
+    assert_eq!(meta.images, vec!["https://example.com/image.jpg"]);
+    assert_eq!(meta.videos, Vec::<String>::new());
+    assert_eq!(meta.hashtags, vec!["#cool"]);
+    assert_eq!(meta.mentions, vec!["@npub1"]);
+  }
 }
