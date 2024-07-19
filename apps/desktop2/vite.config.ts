dcf2791fe59e6255d22b0a4e5b2510f665d35874
reya <123083837+reyamir@users.noreply.github.com>
2024-07-19 09:28:03 +0700

    fix: build

--- apps/desktop2/vite.config.ts
+++ apps/desktop2/vite.config.ts
@@ -1,19 +1,10 @@
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import topLevelAwait from "vite-plugin-top-level-await";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	plugins: [
-		react(),
-		viteTsconfigPaths(),
-		topLevelAwait({
-			promiseExportName: "__tla",
-			promiseImportName: (i) => `__tla_${i}`,
-		}),
-		TanStackRouterVite(),
-	],
+	plugins: [react(), viteTsconfigPaths(), TanStackRouterVite()],
 	build: {
 		outDir: "../../dist",
 	},
