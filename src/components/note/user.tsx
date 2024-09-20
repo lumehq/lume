872a6cee361a3381b44c78a13951369224b83926
reya <reya@lume.nu>
2024-09-20 07:25:07 +0700

    fix: missing context menu in user's avatar

--- src/components/note/user.tsx
+++ src/components/note/user.tsx
@@ -1,47 +1,15 @@
 import { cn } from "@/commons";
-import { LumeWindow } from "@/system";
-import { Menu, MenuItem } from "@tauri-apps/api/menu";
-import { writeText } from "@tauri-apps/plugin-clipboard-manager";
-import { useCallback } from "react";
 import { User } from "../user";
 import { useNoteContext } from "./provider";
 
 export function NoteUser({ className }: { className?: string }) {
 	const event = useNoteContext();
 
-	const showContextMenu = useCallback(async (e: React.MouseEvent) => {
-		e.preventDefault();
-
-		const menuItems = await Promise.all([
-			MenuItem.new({
-				text: "View Profile",
-				action: () => LumeWindow.openProfile(event.pubkey),
-			}),
-			MenuItem.new({
-				text: "Copy Public Key",
-				action: async () => {
-					const pubkey = await event.pubkeyAsBech32();
-					await writeText(pubkey);
-				},
-			}),
-		]);
-
-		const menu = await Menu.new({
-			items: menuItems,
-		});
-
-		await menu.popup().catch((e) => console.error(e));
-	}, []);
-
 	return (
 		<User.Provider pubkey={event.pubkey}>
 			<User.Root className={cn("flex items-start justify-between", className)}>
 				<div className="flex w-full gap-2">
-					<button
-						type="button"
-						onClick={(e) => showContextMenu(e)}
-						className="shrink-0"
-					>
+					<button type="button" className="shrink-0">
 						<User.Avatar className="rounded-full size-8" />
 					</button>
 					<div className="flex items-center w-full gap-3">
